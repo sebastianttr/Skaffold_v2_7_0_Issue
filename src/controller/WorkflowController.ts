6dfe678ce70b7cd61832b7789c5d6a840cef84d7
Sebastian Tatar <st@quanticfinancial.com>
2023-05-09 10:35:21 +0200

    "QC-153 Restart and Guidelines"

--- src/controller/WorkflowController.ts
+++ src/controller/WorkflowController.ts
@@ -2,7 +2,7 @@ import {Controller} from "@tsoa/runtime";
 import {Get, Post, Put, Request, Route} from "tsoa";
 import WorkflowService from "../service/WorkflowService";
 import {Inject, Log} from "../common";
-import {WorkflowStartModel} from "../model/WorkflowStartModel";
+import {WorkFlowProcessModel, WorkflowStartModel} from "../model/WorkflowStartModel";
 import express from "express";
 
 @Route("api/v1/workflow")
@@ -12,6 +12,19 @@ export class WorkflowController extends Controller {
     @Get()
     public async getWorkflowState(@Request() req: express.Request): Promise<any> {
        return WorkflowService.getWorkflowState(req.headers["uid"] as string)
+    }
+
+    @Post()
+    public startWorkflow(@Request() req: express.Request): string {
+        const startModel: WorkflowStartModel = req.body;
+        this.workflowService.startWorkflow(startModel)
+        return "Starting process!";
+    }
+
+    @Post("restart")
+    public restartProcess(@Request() req: express.Request): Promise<string> {
+        const process: WorkFlowProcessModel = req.body;
+        return this.workflowService.restartProcess(process);
     }
 
     @Get("process")
@@ -26,12 +39,4 @@ export class WorkflowController extends Controller {
         const uid: string = req.headers["uid"] as string
         return WorkflowService.getWorkflowProcesses(uid);
     }
-
-    @Post()
-    public startProcess(@Request() req: express.Request): string {
-        const startModel: WorkflowStartModel = req.body;
-        this.workflowService.startProcess(startModel)
-        return "Starting process!";
-    }
-
 }
